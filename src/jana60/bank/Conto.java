66d44a54245e7e103357c5829f8dab97eec1374d
ValerioGc <valerio.gc1@gmail.com>
2022-11-18 10:04:09 +0100

    Init

---
+++ src/jana60/bank/Conto.java
@@ -0,0 +1,58 @@
+package jana60.bank;
+
+import java.util.Random;
+
+public class Conto {
+
+	
+	private int numeroConto;
+	private String nomeProprietario;
+	private int saldo = 0;
+	public char eur = '\u20ac';
+	
+	public Conto() {
+		Random rnd = new Random();
+		this.numeroConto = rnd.nextInt(90000) + 10000;
+	}
+	
+// Getters Setters nome proprietareio
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+	public void setNomeProprietario(String nomeProprietario) {
+		this.nomeProprietario = nomeProprietario;
+	}
+		
+// Getters saldo e conto
+	public int getNumeroConto() {
+		return numeroConto;
+	}
+	public int getSaldo() {
+		return saldo;
+	}
+
+	
+// Metodi prelievo e deposito 
+	public void addMoney(int deposit) {
+		this.saldo += deposit;
+	}
+	public void getMoney(int prelievo) {
+		if (this.saldo - prelievo <= 0) {
+			this.saldo += prelievo;
+		} else {
+			System.out.println("Saldo non sufficiente");
+		}
+	}
+
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Proprietario Conto: " + getNomeProprietario() +
+				"\nNumero Conto: " + getNumeroConto() +
+				"\nIl saldo attuale è di: " + getSaldo() + eur ;
+
+	}
+}
+
---
+++ src/jana60/bank/Main.java
@@ -0,0 +1,30 @@
+package jana60.bank;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Per aprire un conto inserisci il tuo nome");
+		Scanner scn = new Scanner(System.in);
+		String nomeProprietrario = scn.nextLine();
+		Conto conto = new Conto();
+		conto.setNomeProprietario(nomeProprietrario);
+		System.out.println(conto);
+		System.out.println("\n---------------------------\n");
+		System.out.println("Quale operazione vuoi eseguire? (prelievo/deposito)");
+		String operation = scn.nextLine(); 
+		if (operation == "deposito") {
+			System.out.println("Inserisci cifra da depositare");
+			int deposit = scn.nextInt();
+			conto.addMoney(deposit);
+			System.out.println("Operazione completata, saldo attuale: " + conto.getSaldo() + conto.eur);
+		}
+		else if (operation == "prelievo") {
+			System.out.println("Inserisci cifra da prelevare");
+			int prelievo = scn.nextInt();
+			conto.getMoney(prelievo);
+			System.out.println("Operazione completata, saldo attuale: " + conto.getSaldo() + conto.eur);
+		} 
+		scn.close();
+	}
+}
